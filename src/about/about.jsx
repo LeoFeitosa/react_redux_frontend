6b4f4fb5c04581f8231d4f963d394b13246f29a4
Leonardo Feitosa <leonardo.feitosa@uniesp.edu.br>
2018-05-22 17:34:02 -0300

    Inicial

---
+++ src/about/about.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import PageHeader from '../template/pageHeader'
+
+export default props => (
+  <div>
+    <PageHeader name="Sobre" small="Nós" />
+
+    <h2>Nossa história</h2>
+    <p>Mussum Ipsum, cacilds vidis litro abertis. Não sou faixa preta cumpadi, sou preto inteiris, inteiris. Nullam volutpat risus nec leo commodo, ut interdum diam laoreet. Sed non consequat odio. Sapien in monti palavris qui num significa nadis i pareci latim. Suco de cevadiss, é um leite divinis, qui tem lupuliz, matis, aguis e fermentis.</p>
+    <h2>Missão e Visão</h2>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatum, recusandae!</p>
+    <h2>Imprensa</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione temporibus tempora, minus nisi, commodi quasi ex perferendis voluptatibus perspiciatis, architecto eaque. Laboriosam ipsam, aliquid distinctio illo veritatis dolorum ducimus enim!</p>
+  </div>
+)
