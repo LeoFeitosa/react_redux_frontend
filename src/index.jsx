7cf08231bec5ef0454f1d96249f60e809100d179
Leonardo Feitosa <leonardo.feitosa@uniesp.edu.br>
2018-05-23 10:31:11 -0300

    coreção na inclusão do redux

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { createStore } from 'redux'
+
 import App from './main/app'
 
 ReactDOM.render(<App />, document.getElementById('app'))
