9645791e5b9906e4eec594010581ebe0a1d9c1ac
Leonardo Feitosa <leonardo.feitosa@uniesp.edu.br>
2018-05-23 10:55:38 -0300

    listando tarefas com reducers

--- src/todo/todo.jsx
+++ src/todo/todo.jsx
@@ -71,7 +71,7 @@ export default class Todo extends Component {
           handleAdd={this.handleAdd}
           handleSearch={this.handleSearch}
           handleClear={this.handleClear} />
-        <TodoList list={this.state.list}
+        <TodoList
           handleRemove={this.handleRemove}
           handleMarkAsDone={this.handleMarkAsDone}
           handleMarkAsPending={this.handleMarkAsPending} />
