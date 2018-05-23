eedebd609680e2d7e7eb3a84342b06ce9e2c66fb
Leonardo Feitosa <leonardo.feitosa@uniesp.edu.br>
2018-05-23 11:31:49 -0300

    final form com reducer/action

--- src/main/reducers.js
+++ src/main/reducers.js
@@ -1,22 +1,8 @@
 import { combineReducers } from 'redux'
+import todoReducer from '../todo/todoReducer'
 
 const rootReducer = combineReducers({
-  todo: () => ({
-    description: 'ler livro',
-    list: [{
-      _id: 1,
-      description: 'pagar fatura',
-      done: true
-    }, {
-      _id: 2,
-      description: 'Reunião',
-      done: false
-    }, {
-      _id: 3,
-      description: 'Consulta medica',
-      done: false
-    }]
-  })
+  todo: todoReducer
 })
 
 export default rootReducer
---
+++ src/todo/todoReducer.js
@@ -0,0 +1,25 @@
+const INITIAL_STATE = {
+  description: 'ler livro',
+  list: [{
+    _id: 1,
+    description: 'pagar fatura',
+    done: true
+  }, {
+    _id: 2,
+    description: 'Reunião',
+    done: false
+  }, {
+    _id: 3,
+    description: 'Consulta medica',
+    done: false
+  }]
+}
+
+export default (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    case 'DESCRIPTION_CHANGED':
+      return { ...state, description: action.payload }
+      default:
+        return state
+  }
+}
