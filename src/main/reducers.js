772ad1b0877f38f84eed008c7e1be16940642198
Leonardo Feitosa <leonardo.feitosa@uniesp.edu.br>
2018-05-23 10:36:45 -0300

    reducer statico

--- src/main/reducers.js
+++ src/main/reducers.js
@@ -1,1 +1,22 @@
 import { combineReducers } from 'redux'
+
+const rootReducer = combineReducers({
+  todo: () => ({
+    description: 'ler livro',
+    list: [{
+      _id: 1,
+      description: 'pagar fatura',
+      done: true
+    }, {
+      _id: 2,
+      description: 'Reunião',
+      done: false
+    }, {
+      _id: 3,
+      description: 'Consulta medica',
+      done: false
+    }]
+  })
+})
+
+export default rootReducer
