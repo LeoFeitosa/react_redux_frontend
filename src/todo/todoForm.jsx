992c1f6c20a6589791eb053eb793bc1f84eb3a49
Leonardo Feitosa <leonardo.feitosa@uniesp.edu.br>
2018-05-23 11:19:35 -0300

    ligação do form com a action

--- src/todo/todoForm.jsx
+++ src/todo/todoForm.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
 
 import Grid from '../template/grid'
 import IconButton from '../template/iconButton'
+import { changeDescription } from './todoActions'
 
 const TodoFrom = props => {
   const keyHandler = (e) => {
@@ -20,7 +22,7 @@ const TodoFrom = props => {
       <input id="description" className="form-control"
         placeholder="Adicione uma tarefa"
         value={props.description}
-        onChange={props.handleChange}
+        onChange={props.changeDescription}
         onKeyUp={keyHandler}/>
     </Grid>
 
@@ -38,4 +40,7 @@ const mapStateToProps = state => ({
   description: state.todo.description
 })
 
-export default connect(mapStateToProps)(TodoFrom)
+const mapDispatchToProps = dispatch =>
+  bindActionCreators({ changeDescription }, dispatch)
+
+export default connect(mapStateToProps, mapDispatchToProps)(TodoFrom)
