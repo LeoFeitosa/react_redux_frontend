10933aff8814cbc8d45849f09bffedc6946eafc6
Leonardo Feitosa <leonardo.feitosa@uniesp.edu.br>
2018-05-23 16:38:55 -0300

    utilizando react-thunk para add e pending

--- src/todo/todoList.jsx
+++ src/todo/todoList.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+
 import IconButton from '../template/iconButton'
+import { markAsDone, markAsPending } from './todoActions'
 
 const TodoList = props => {
   const renderRows = () => {
@@ -10,10 +13,10 @@ const TodoList = props => {
         <td className={todo.done ? "markedAsDone" : ""}>{todo.description}</td>
         <td>
           <IconButton style="success" icon="check" hide={todo.done}
-            onClick={() => props.handleMarkAsDone(todo)} />
+            onClick={() => props.markAsDone(todo)} />
 
           <IconButton style="warning" icon="undo" hide={!todo.done}
-            onClick={() => props.handleMarkAsPending(todo)} />
+            onClick={() => props.markAsPending(todo)} />
 
           <IconButton style="danger" icon="trash-o" hide={!todo.done}
             onClick={() => props.handleRemove(todo)} />
@@ -41,4 +44,6 @@ const mapStateToProps = state => ({
   list: state.todo.list
 })
 
-export default connect(mapStateToProps)(TodoList)
+const mapDispatchToProps = (dispatch) => bindActionCreators({ markAsDone, markAsPending }, dispatch)
+
+export default connect(mapStateToProps, mapDispatchToProps)(TodoList)
